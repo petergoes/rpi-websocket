07bde1eeb6094e255ca04edce246d5c503750b36
Peter Goes <petergoes@gmail.com>
2016-06-29 21:17:21 +0200

    feat: add socket.io

--- index.js
+++ index.js
@@ -7,6 +7,6 @@ const io = require('socket.io').listen(server);
 server.listen('3000');
 app.use(express.static('public'));
 
-io.sockets.on('connection', (socket) => {
-	
+io.on('connection', (socket) => {
+	console.log('on connection');
 });
